c316338315515581f01700e45d55c943075fb62f
yogitasinkar <yogita.sinkar5@gmail.com>
2020-09-14 16:36:37 +0530

    Updates

---
+++ precache-manifest.b7dffc27f4f8bbadf4782e76126320f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51fd462d6e2d8701acb2363ff007ee19",
+    "url": "/countdown-timer-boilerplate/index.html"
+  },
+  {
+    "revision": "fceca1aa1cf012c07913",
+    "url": "/countdown-timer-boilerplate/static/css/main.6e9f9e14.chunk.css"
+  },
+  {
+    "revision": "e4d7108380b3255135f2",
+    "url": "/countdown-timer-boilerplate/static/js/2.74ed5750.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/countdown-timer-boilerplate/static/js/2.74ed5750.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fceca1aa1cf012c07913",
+    "url": "/countdown-timer-boilerplate/static/js/main.3e800c36.chunk.js"
+  },
+  {
+    "revision": "3b9a2e4c3d48c28dd7d1",
+    "url": "/countdown-timer-boilerplate/static/js/runtime-main.6ef87fe9.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/countdown-timer-boilerplate/static/media/logo.ee7cd8ed.svg"
+  }
+]);
